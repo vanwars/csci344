4e5d9e26270cb9c113900821ecf0820fa950967d
Sarah <vanwars@gmail.com>
2023-02-10 14:42:37 -0500

    Work so far

---
+++ tutorials/tutorial05/main.js
@@ -0,0 +1,101 @@
+// Part 1: Set up the helper functions:
+// 1. Implement two filter functions (which should return either true or false):
+//      * filterClassFull: to filter out the closed courses (if applicable)
+//      * filterTermMatched: to only match courses relevant to the search term
+// 2. Implement the dataToHTML function, which takes a course object as an
+//    argument and returns an HTML string that represents the course.
+
+// Part 2: Within the showData function, use the array's filter, map, join
+//         methods, and any relevant DOM methods, to build the interface.
+// 1. Use the array's built in "filter" method, which takes a filter
+//    function as an argument and returns an array of objects that 
+//    match the criteria.
+//          * Note that you can chain filter functions together.
+// 2. Use the array's built in "map" method to generate an array of 
+//    HTML strings.
+// 3. Join the array of strings on the empty string or new line character
+//    to create one large HTML string.
+// 4. Clear out the existing courses in the DOM and insert
+//    the HTML string into the DOM.
+
+const search = ev => {
+    ev.preventDefault(); // overrides default button action
+
+    // Get user's preferences:
+    const searchTerm = document.querySelector('#search_term').value;
+    const openOnly = document.querySelector('#is_open').checked;
+    
+    // Pass the user's preferences into the showData function
+    showData(searchTerm, openOnly);
+}
+
+// Part 1.1a
+const filterClassFull = course => {
+    // modify this
+    return true;
+}
+
+// Part 1.1b
+const filterTermMatched = course => {
+    // modify this
+    return true;
+}
+
+// Part 1.2
+const dataToHTML = course => {
+    // modify this
+    return `
+        <section class="course">
+            <h2>${ course.Code}: ${ course.Title }</h2>
+            <p>
+                <i class="fa-solid fa-circle-check"></i> 
+                Open  &bull; 10174 &bull; Seats Available: 1
+            </p>
+            <p>
+                MWF &bull; ZEI 201 &bull; 3 credit hour(s)
+            </p>
+            <p><strong>Whitley, Adam</strong></p>
+        </section>
+    `;
+}
+
+// Part 2
+const showData = (searchTerm, openOnly) => {
+    console.log(searchTerm, openOnly);
+    console.log(data); // imported from course-data.js
+    // Your code here:
+    /*
+    1. filter out by search term
+    2. filter out by whether open or closed
+    3. take the matched courses and convert them all to an HTML array.
+    4. Join that array of strings into a megastring
+    5. Insert megastring into the DOM.
+    */
+
+    // behind the scenes, "filter" method is invoking the "filterTermMatched(item)"
+    // function on every item in the array.
+    const dataThatMatchesQuery = data.filter(filterTermMatched);
+    console.log("List of data matching query:", dataThatMatchesQuery);
+
+    const onlyShowOpenClassesIfSpecified = dataThatMatchesQuery.filter(filterClassFull);
+    console.log("List of open classes (if specified):", onlyShowOpenClassesIfSpecified);
+
+    const listOfHTMLChunks = onlyShowOpenClassesIfSpecified.map(dataToHTML);
+    console.log("List of Strings:", listOfHTMLChunks);
+
+    const megaString = listOfHTMLChunks.join('\n');
+    console.log(megaString);
+
+    document.querySelector('.courses').insertAdjacentHTML('beforeend', megaString);
+
+    // document.querySelector('.courses').insertAdjacentHTML(
+    //     'beforeend', data.filter(
+    //         filterTermMatched
+    //     ).filter(
+    //         filterClassFull 
+    //     ).map(
+    //         dataToHTML
+    //     ).join('')
+    // );
+    
+}
