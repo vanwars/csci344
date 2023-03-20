9a4eb648f6bf6646ab37769128bc9edb16996892
Sarah <vanwars@gmail.com>
2023-03-20 14:44:31 -0400

    edits

--- tutorials/tutorial08/src/LikeButton.js
+++ tutorials/tutorial08/src/LikeButton.js
@@ -7,25 +7,24 @@ export default function LikeButton({post}) {
     const likeId = post.current_user_like_id;
     const postId = post.id;
 
-    function likeUnlike() {
+    async function likeUnlike() {
         console.log(likeId, postId);
         // if it's liked, unlike it, else like it
         if (likeId) {
             console.log('unlike!')
         } else {
+            // code to like a post:
             console.log('like!')
             const postData = {
                 "post_id": postId
             };
-            fetch("/api/posts/likes/", {
+            const response = await fetch("/api/posts/likes/", {
                 method: "POST",
                 headers: getHeaders(token),
                 body: JSON.stringify(postData)
-            })
-            .then(response => response.json())
-            .then(data => {
-                console.log(data);
             });
+            const data = await response.json();
+            console.log(data);
         }
     }
 
