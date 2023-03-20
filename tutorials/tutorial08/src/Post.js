27cfa589916de079d6a1309bd69479846d801db7
Sarah <vanwars@gmail.com>
2023-03-20 15:08:33 -0400

    updates

--- tutorials/tutorial08/src/Post.js
+++ tutorials/tutorial08/src/Post.js
@@ -3,19 +3,35 @@
 
 import React from 'react';
 import LikeButton from './LikeButton';
+import {getHeaders} from './utils';
 
+import { useState } from "react";
 
 export default function Post({post, token}) {
 
+    const [actualPost, setActualPost] = useState(post);
 
+
+    async function requeryPost() {
+        // get a fresh copy of the post
+            const response = await fetch(`/api/posts/${post.id}`, {
+            method: "GET",
+            headers: getHeaders(token)
+        });
+        const data = await response.json();
+        console.log(data);
+        // to make the screen redraw after requerying the post,
+        // we need to set a state variable:
+        setActualPost(data);
+    }
    
     // JSX representation of a Post
     return (
         <section className="card">
-            <img src={post.image_url} alt={post.caption} />
-            <div>{post.caption}</div>
+            <img src={actualPost.image_url} alt={actualPost.caption} />
+            <div>{actualPost.caption}</div>
             <div className="buttons">
-                <LikeButton post={post} token={token} />
+                <LikeButton post={actualPost} token={token} />
             </div>
         </section> 
     )  
