72b987809c9099d65be2012bcd619988dcf561ac
Sarah <vanwars@gmail.com>
2024-08-30 13:46:23 -0400

    Uploading Tutorial 2

---
+++ tutorials/tutorial02b/main.js
@@ -0,0 +1,33 @@
+let canvasWidth = window.innerWidth;
+let canvasHeight = window.innerHeight;
+
+// in p5.js, the function runs on page load:
+function setup() {
+    createCanvas(canvasWidth, canvasHeight);
+
+    // invoke any drawing functions inside of setup.
+    // functions should all go between "createCanvas()" and "drawGrid()"
+    draw5Circles();
+    // draw5RedSquares();
+    drawGrid(canvasWidth, canvasHeight);
+}
+
+// my first function
+function draw5Circles() {
+    noFill();
+    // fill('red');
+    circle(100, 200, 50); // centerX, centerY, radius
+    circle(100, 250, 50);
+    circle(100, 300, 50);
+    circle(100, 350, 50);
+    circle(100, 400, 50);
+}
+
+function draw5RedSquares() {
+    fill("red");
+    square(320, 200, 50); // topLeftX, topLeftY, width
+    square(320, 250, 50);
+    square(320, 300, 50);
+    square(320, 350, 50);
+    square(320, 400, 50);
+}
