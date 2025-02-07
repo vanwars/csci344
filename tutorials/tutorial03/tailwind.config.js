fcc27a1988bb4fbf458cc7e6d302fcea5e9ce1cc
Sarah <svanwart@unca.edu>
2025-02-07 12:20:38 -0500

    Publishing homepage

---
+++ tutorials/tutorial03/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    content: ["./*html"],
+    theme: {
+        extend: {
+            colors: {
+                "custom-blue": "#00BAFF",
+                "custom-purple": "#6336FA",
+            },
+        },
+        fontFamily: {
+            Poppins: ["Poppins"],
+        },
+    },
+    plugins: [],
+};
