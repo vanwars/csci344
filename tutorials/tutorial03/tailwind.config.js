823082569ce6268e635e1da2daa64e3129622b31
Sarah <vanwars@gmail.com>
2026-02-05 19:05:02 -0500

    Tutorial 3

---
+++ tutorials/tutorial03/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    content: ["./*.html", "./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                "custom-blue": "#00BAFF",
+                "custom-purple": "#6336FA",
+            },
+            fontFamily: {
+                Poppins: ["Poppins", "sans-serif"],
+            },
+        },
+    },
+    plugins: [],
+};
